810a80eb5c2ab3a5328a72ea37cfe3a29cb8376b
Kai Widmann <widmann@mediaworx.com>
2014-06-16 12:16:01 +0200

    Extended Javadoc, parameter rename (no functional change)

--- src/main/java/com/mediaworx/opencms/moduleutils/packager/exceptions/OpenCmsModulePackagerException.java
+++ src/main/java/com/mediaworx/opencms/moduleutils/packager/exceptions/OpenCmsModulePackagerException.java
@@ -1,12 +1,24 @@
 package com.mediaworx.opencms.moduleutils.packager.exceptions;
 
+/**
+ * Exception used whenever there's an Error while packaging OpenCms modules
+ */
 public class OpenCmsModulePackagerException extends Exception {
 
+	/**
+	 * Creates a new OpenCmsModulePackagerException
+	 * @param s description of the error
+	 */
 	public OpenCmsModulePackagerException(String s) {
 		super(s);
 	}
 
-	public OpenCmsModulePackagerException(String s, Throwable throwable) {
-		super(s, throwable);
+	/**
+	 * Creates a new OpenCmsModulePackagerException with a throwable that was causing the exception
+	 * @param s description of the error
+	 * @param cause the root cause of the Exception
+	 */
+	public OpenCmsModulePackagerException(String s, Throwable cause) {
+		super(s, cause);
 	}
 }
